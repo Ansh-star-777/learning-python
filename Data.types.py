e517de22927d120425097659a35216c8d24c0b24
Ansh-star-777 <sangeetasiwach04@gmail.com>
2026-04-13 19:44:24 +1200

    Add type checking examples in Data.types.py

---
+++ Data.types.py
@@ -0,0 +1,13 @@
+a = 5
+b = 7.5
+print(type(a))
+print(type(b))
+
+
+c = input("Enter a number")
+j = int(input("Enter a number_typecasting"))
+print(c)
+print(type(c))
+print(type(j))
+d = 3
+print(type(67))
