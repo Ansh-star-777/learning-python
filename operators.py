7614979b5f1d879aee3b52a79713be3c6554e404
Ansh-star-777 <sangeetasiwach04@gmail.com>
2026-04-15 19:41:57 +1200

    Implement basic operator examples in operators.py
    Added examples of arithmetic, comparison, and assignment operators.

---
+++ operators.py
@@ -0,0 +1,28 @@
+# Arthmetic Operators
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
+print(5 % 2)
+
+
+# comparison operators
+print(5 == 2)
+print(5 == 5) # equality operator
+print(5 != 2) # not equal to operator
+print(5 != 5) # not equal to operator
+print(5 > 2)
+print(5 < 2)
+print(5 < 5)
+print(5 <= 5) # less than or equal to operator
+
+
+# assignment operator
+a = 5
+print("value of a:",a)
+a += 2 # this can also be written as: a = a + 2
+print("new value of a:",a)
+a -= 2
+a *= 2
+a /= 2
+a %= 2
