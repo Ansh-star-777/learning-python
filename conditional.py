3965afd06568cd61f4104d29bdda13cb58202039
Ansh-star-777 <sangeetasiwach04@gmail.com>
2026-04-17 17:41:35 +1200

    Add conditional checks for number comparisons

---
+++ conditional.py
@@ -0,0 +1,13 @@
+a = int(input("enter a number for comparing"))
+print(type(a))
+if a > 50:
+    print("number is greater than 50")
+else:
+    print("number is less than 50")
+
+
+b = int(input("Enter a Number"))
+if b > 0:
+    print("Number is positive")
+else:
+    print("Number is negative")
