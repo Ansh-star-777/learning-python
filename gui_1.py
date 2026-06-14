a20b3554191a932df7ccae20310bf05a1c01b823
Ansh-star-777 <sangeetasiwach04@gmail.com>
2026-06-14 17:41:08 +1200

    Add basic Tkinter GUI with a greeting label

---
+++ gui_1.py
@@ -0,0 +1,13 @@
+from tkinter import *
+# import tkinter as tk
+
+window = Tk()
+window.title("Aura-67")
+window.geometry("400x200")
+
+label = Label(window,text="hello ansh",fg="white",bg="black",borderwidth=3,height=20,width=50)
+label.pack(padx=0,pady=0)
+
+
+
+window.mainloop()
